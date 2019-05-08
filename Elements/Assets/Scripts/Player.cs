e4092c6cc0fdee84b26678097e9630be3fd72e27
danielc279 <43954906+danielc279@users.noreply.github.com>
2019-05-08 20:02:03 +0200

    Initial commit

---
+++ Elements/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D myRigidbody;
+
+	[SerializeField]
+	private float movementSpeed;
+
+	private bool facingRight;
+
+	void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		HandleMovement(horizontal);
+
+		Flip(horizontal);
+	}
+
+	private void HandleMovement(float horizontal)
+	{
+		myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y); //x-1, y = 0;
+	}
+
+	private void Flip(float horizontal)
+	{
+		if (horizontal > 0 & facingRight || horizontal < 0 && !facingRight)
+		{
+			facingRight = !facingRight;
+
+			Vector3 theScale = transform.localScale;
+
+			theScale.x *= -1;
+
+			transform.localScale = theScale;
+		}
+	}
+}
