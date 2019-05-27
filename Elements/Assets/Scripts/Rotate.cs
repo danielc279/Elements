8dd7d479c643de7202990d9dd5ad1d59e64c3dfc
danielc279 <43954906+danielc279@users.noreply.github.com>
2019-05-27 21:10:45 +0200

    Finished Rotate
    - Level now rotates around the player at 90 degrees
    - The player cannot move and floats whilst the rotation happens.

--- Elements/Assets/Scripts/Rotate.cs
+++ Elements/Assets/Scripts/Rotate.cs
@@ -5,37 +5,56 @@ using UnityEngine;
 public class Rotate : MonoBehaviour
 {
 // duration of the rotation in seconds, can be set via Inspector
-public float RotationDuration = 0.5f;
-private bool _isRotating = false;
+
+
+private bool is_Rotating = false;
+private GameObject player;
+private Rigidbody2D myRB;
+
+private Player myPlayer;
+
+private Animator myAnimator;
+private float time;
+
+void Start()
+{
+
+    player = GameObject.Find("Player");
+    myRB = player.gameObject.GetComponent<Rigidbody2D>();
+    myPlayer = player.GetComponent<Player>();
+    myAnimator = player.GetComponent<Animator>();
+}
 
 void Update () {
-    if (Input.GetKeyDown(KeyCode.C) && !_isRotating) { 
-        StartCoroutine(RotateObject(
-            transform.rotation, 
-            transform.rotation * Quaternion.Euler(0, 0, 90), 
-            RotationDuration
-        ));
+    if (Input.GetKeyDown(KeyCode.C) && !is_Rotating) { 
+        StartCoroutine(RotateObject(1, 1, 89));
     }
-    if (Input.GetKeyDown(KeyCode.V) && !_isRotating) {
-        StartCoroutine(RotateObject(
-            transform.rotation, 
-            transform.rotation * Quaternion.Euler(0, 0, -90), 
-            RotationDuration
-        ));
+    if (Input.GetKeyDown(KeyCode.V) && !is_Rotating) {
+        StartCoroutine(RotateObject(1, -1,-89));
     }
 }
 
-IEnumerator RotateObject(Quaternion start, Quaternion end, float duration)
+IEnumerator RotateObject(float rotateTime, float offset, float rotateAmount)
 {
-    float endTime = Time.time + duration;
-    _isRotating = true;
-    while (Time.time <= endTime) {
-        // normalized progress from 0 - 1
-        float t = 1f - (endTime - Time.time) / duration;
-        transform.rotation = Quaternion.Lerp(start, end, t);
-        yield return 0;
+    myRB.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+    myPlayer.enabled = false;
+    myAnimator.enabled = false;
+    Quaternion tmpRotation = transform.rotation;
+    is_Rotating = true;
+    while (time < rotateTime)
+    {
+    float timePassed = Time.deltaTime;
+    time += timePassed;
+    transform.RotateAround(player.transform.GetChild(1).transform.position, Vector3.forward, rotateAmount * timePassed);
+    yield return new WaitForSeconds(timePassed/1.5f);
     }
-    transform.rotation = end;
-    _isRotating = false;
+    is_Rotating = false;
+    transform.RotateAround(player.transform.GetChild(1).transform.position, Vector3.forward, tmpRotation.eulerAngles.z + rotateAmount + offset - transform.rotation.eulerAngles.z);
+    time = 0.0f;
+    myRB.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeRotation;
+    myPlayer.enabled = true;
+    myAnimator.enabled = true;
+    yield return null;
 }
+
 }
